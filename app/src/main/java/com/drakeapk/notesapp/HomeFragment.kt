8cb9c4720ce4244a181d18406d28e0f2d4616a0d
Drake343 <87871563+Drake343@users.noreply.github.com>
2021-11-14 14:35:35 +0530

    adding dependencies Notes App layout

---
+++ app/src/main/java/com/drakeapk/notesapp/HomeFragment.kt
@@ -0,0 +1,36 @@
+package com.drakeapk.notesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
